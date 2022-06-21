12552d7dce8142c24430c9d6e09a0ee4093564a8
Azhar <Azharthegreat123@gmail.com>
2022-06-22 00:33:02 +0530

    added two pages with and added their routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import './App.css'
+import { Route, Routes} from 'react-router-dom';
+import { Box } from '@mui/material';
+import Home from '../src/pages/Home';
+import ExerciseDetail from '../src/pages/ExerciseDetail';
+import NavBar from '../src/components/NavBar';
+import Footer from '../src/components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Box width="400px">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
