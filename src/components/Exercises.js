93ad8acb25a49fa794f8e292a83c47ee42851b2a
Azhar uddin sheikh <azharsheikh760@gmail.com>
2022-07-30 14:22:40 +0530

    adding pagination on Exercise list

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -6,6 +6,18 @@ import ExerciseCard from './ExerciseCard'
 
 
 const Exercises = ({setExercises, bodyPart, exercises}) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 8;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise)
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+    window.scrollTo({top:1800, behavior:'smooth'})
+  }
+
   return (
     <Box 
       id='exercises'
@@ -18,7 +30,16 @@ const Exercises = ({setExercises, bodyPart, exercises}) => {
             sx={{gap:{lg:'110px', x5:'50px'}}}
             flexWrap="wrap"
             justifyContent="center">
-              {exercises.map((exercise, index) => <ExerciseCard key={index} exercise={exercise}/> )}
+              {currentExercises.map((exercise, index) => <ExerciseCard key={index} exercise={exercise}/> )}
+            </Stack>
+            <Stack mt='100px' alignItems='center'>
+              {exercises.length > exercisesPerPage && (
+                <Pagination color='standard' shape='rounded'
+                  defaultPage={1} count={Math.ceil(exercises.length/exercisesPerPage)}
+                  page={currentPage}
+                  onChange={paginate}
+                  size="large" />
+              )}
             </Stack>
         </Typography>
     </Box>
