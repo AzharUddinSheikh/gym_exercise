3a0cd65ff3321e3af18bf9f71b422d494783f3dd
Azhar uddin sheikh <azharsheikh760@gmail.com>
2022-07-30 14:35:21 +0530

    fully functional home page, bodyPart component render Exercises

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -18,6 +18,21 @@ const Exercises = ({setExercises, bodyPart, exercises}) => {
     window.scrollTo({top:1800, behavior:'smooth'})
   }
 
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      let exercisesData = [];
+      if (bodyPart === 'all') {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', 
+                        exerciseOptions);
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, 
+                        exerciseOptions);
+      }
+      setExercises(exercisesData);
+    }
+    fetchExerciseData();
+  }, [bodyPart])
+
   return (
     <Box 
       id='exercises'
